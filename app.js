a5033cb753bfc673c2563ed00704b1dc2fd2bf21
CONOR WOODS <CONOR WOODS>
2016-09-16 18:11:49 +0100

    added jquery

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
-function checkedLoggedIn(){
+"use strict";
+var auth = auth();
+document.getElementById('login').click(function () {
+
+    var email = $("#username").val();
+    var password = $("#password").val();
+    // log out before we try to log in again
+    auth.logout();
+    auth.login(email,password,successFunction,errorFunction);
+
+});
+
+function successFunction(){
+    checkedLoggedIn();
+}
+function errorFunction(){
+    checkedLoggedIn();
+}
+
+$("#logout").click(function () {
+    auth.logout();
+    location.reload(true);
+});
+// check to see if a cookie already exists and use it if present
+function checkLoggedIn() {
     var cookie = auth.getLoggedInCookie();
     if (cookie) {
-        cookie = JSON.parse(cookie);
         $("#login").hide();
-        $("#register").hide();
         $("#logindetails").hide();
         $("#logout").show();
         $("#currentuser").text(cookie.user_display_name);
@@ -15,3 +37,5 @@ function checkedLoggedIn(){
         $("#currentuser").text('');
     }
 }
+// execute immediately to hide or show buttons
+checkLoggedIn();
--- auth.js
+++ auth.js
@@ -1,10 +1,54 @@
+"use strict";
+function auth() {
 
-function login(){
+    var COOKIE_NAME = 'mycookiename';
 
+    function login(email, password, success, failure) {
+
+        var apiHost = '//localhost/wordpress/wp-json';
+
+        var data = {
+            username: email,
+            password: password
+        };
+        $.post(apiHost + '/jwt-auth/v1/token', data).done(function (data) {
+            // you are now logged in, save the cookie to validate further requests
+            var jwtDetails = JSON.stringify(data);
+            console.log(jwtDetails);
+            // expire our cookie in 7 days
+            createCookie(COOKIE_NAME, jwtDetails, 7);
+            success(jwtDetails);
+        });
+
+    }
+    function logout() {
+
+    }
+    function delete_cookie(name) {
+        document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;path=/';
+    }
+
+    function createCookie(name, value, days) {
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            var expires = "; expires=" + date.toGMTString();
+        } else var expires = "";
+        document.cookie = name + "=" + value + expires + "; path=/";
+    }
+    function getCookie(name) {
+        var value = "; " + document.cookie;
+        var parts = value.split("; " + name + "=");
+        if (parts.length == 2) return parts.pop().split(";").shift();
+    }
+
+    function getLoggedInCookie() {
+        return getCookie(COOKIE_NAME);
+    }
+
+    return {
+        getLoggedInCookie: getLoggedInCookie,
+        login: login,
+        logout: logout
 }
-function logout(){
-
-}
-function isLoggedIn(){
-
-}
+};
